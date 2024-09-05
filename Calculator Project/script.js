f2e7d50c840b1c04441a0e608cb2d879f9a0f634
isiahw <99304805+isiahw@users.noreply.github.com>
2024-09-04 20:22:45 -0400

    Add files via upload

---
+++ Calculator Project/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+let num1 = Number(prompt("enter number 1"))
+console.log(num1);
+
+let num2 = Number(prompt("enter number 2"))
+console.log(num2);
+
+let myAddition = num1 + num2
+console.log("your addition is: " + myAddition)
